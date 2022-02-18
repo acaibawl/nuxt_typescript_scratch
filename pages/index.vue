6e8005d3111a1ef29871fc68ab97dcc331d5e8d1
acaiibawl <hagiya0719@gmail.com>
2022-02-18 10:54:57 +0900

    手動でpackage.jsonを作成し、nuxtとtypescriptをインストール

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ helloWorld }}</p>
+</template>
+
+<script lang="ts">
+  import { Component, Vue} from 'nuxt-property-decorator';
+
+  @Component
+  export default class Index extends Vue {
+    get helloWorld(): string {
+      return 'Hello world!!!!';
+    }
+  }
+</script>
